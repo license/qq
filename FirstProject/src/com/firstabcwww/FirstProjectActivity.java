e4475343827cf6ebd44bcaafd04f9e5650c2190d
Administrator <Administrator@AndBiz>
2011-08-13 12:04:27 +0800

    QQQ

---
+++ FirstProject/src/com/firstabcwww/FirstProjectActivity.java
@@ -0,0 +1,13 @@
+package com.firstabcwww;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+} 
